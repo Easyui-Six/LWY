bc2f0d533fa24a39f54aa47d97aa12b204bb1008
Easyui-Six <42995642+Easyui-Six@users.noreply.github.com>
2021-07-15 10:45:21 +0800

    Add files via upload

---
+++ LWY.Api3.0/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Autofac.Extensions.DependencyInjection;
+using LWY.Service;
+
+namespace LWY.Api3._0
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                //改用Autofac来实现依赖注入
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
