56dbc6697a4abd585fb916e8a686ee8e4ac43495
Easyui-Six <42995642+Easyui-Six@users.noreply.github.com>
2021-07-15 10:54:59 +0800

    Add files via upload

---
+++ LWY.FW.BLL/SysManager/ISysManagerService.cs
@@ -0,0 +1,17 @@
+﻿
+using LWY.FW.Common;
+using LWY.FW.Models;
+using LWY.FW.Models.Entity;
+
+
+namespace LWY.FW.BLL
+{
+    public interface ISysManagerService
+    {
+        BaseWebApiResponse<int> AddUser(User entity);
+
+        BaseWebApiResponse<int> AddFamily(Family family);
+
+        string GetStr();
+    }
+}
---
+++ LWY.FW.BLL/SysManager/SysManagerServiceImpl.cs
@@ -0,0 +1,34 @@
+﻿
+using LWY.FW.Common;
+using LWY.FW.DAL;
+using LWY.FW.Models.Entity;
+
+namespace LWY.FW.BLL
+{
+    public class SysManagerServiceImpl:ISysManagerService
+    {
+        #region   注入
+        public IRepository<User> UserRepository { get; set; }
+
+        public IRepository<Family> FamilyRepository { get; set; }
+
+        #endregion 
+
+        public BaseWebApiResponse<int> AddUser(User user)
+        {
+            User entity = UserRepository.Add(user);
+            return new BaseWebApiResponse<int>(entity.ID);
+        }
+
+        public BaseWebApiResponse<int> AddFamily(Family family)
+        {
+            Family entity = FamilyRepository.Add(family);
+            return new BaseWebApiResponse<int>(0);
+        }
+
+        public string GetStr()
+        {
+            return "123";
+        }
+    }
+}
